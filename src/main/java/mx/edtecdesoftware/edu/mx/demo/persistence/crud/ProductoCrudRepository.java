9c20a1f66533c230dfeff237bab25a7e86e9c6d5
Joshua-221 <jc7909861@gmail.com>
2026-06-02 21:04:16 -0600

    feat: implement ProductoRepository and custom query methods

--- src/main/java/mx/edtecdesoftware/edu/mx/demo/persistence/crud/ProductoCrudRepository.java
+++
@@ -1,8 +0,0 @@
-package mx.edtecdesoftware.edu.mx.demo.persistence.crud;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface ProductoCrudRepository<Producto>
-        extends CrudRepository<Producto, Integer> {
-
-}
--- src/main/java/mx/edu/tecdesoftware/market_backend_2026_3_a/persistence/ProductoRepository.java
+++ src/main/java/mx/edu/tecdesoftware/market_backend_2026_3_a/persistence/ProductoRepository.java
@@ -2,15 +2,40 @@ package mx.edu.tecdesoftware.market_backend_2026_3_a.persistence;
 
 import mx.edu.tecdesoftware.market_backend_2026_3_a.persistence.crud.ProductoCrudRepository;
 import mx.edu.tecdesoftware.market_backend_2026_3_a.persistence.entity.Producto;
+
+
 import java.util.List;
+import java.util.Optional;
 
 public class ProductoRepository {
-
     private ProductoCrudRepository productoCrudRepository;
 
-    //SELECT * FROM productos
-    public list<Producto> getAll(){
-        //Se "castear" de Iterable a la Lista
+    // SELECT * FROM productos
+    public List<Producto> getAll() {
         return (List<Producto>) productoCrudRepository.findAll();
+    }
+
+    public List<Producto> getByCategoria(int idCategoria) {
+        return productoCrudRepository.findbyIdCategoriaOrderByNombreAsc(idCategoria);
+
+    }
+    /*
+     SELECT *
+     FROM producto
+     WHERE cantidad_stock <?
+     AND estado = true
+      */
+    public Optional<List<Producto>> getEscasos(int cantidad){
+        return productoCrudRepository.findByCantidadStockLessThanAndEstado(cantidad,true);
+    }
+
+    //Obtener un producto dado el id
+    public Optional<Producto> getProductoById(int idProducto) {
+        return productoCrudRepository.findById(idProducto);
+    }
+
+    //Guardar un producto
+    public Producto save(Producto producto) {
+        return (Producto) productoCrudRepository.save(producto);
     }
 }
